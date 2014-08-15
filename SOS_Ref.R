04db81462a03566321048a30d385157b813f0a8c
seanlhodges <seanlhodges@gmail.com>
2014-08-15 14:39:01 +1200

    Added functions to function library to support map creation and generation of timeseries googlecharts

--- SOS_Ref.R
+++ SOS_Ref.R
@@ -98,18 +98,18 @@ rcLastTVP <- function(df,measurement){
             
             ## Using some Kister and Waikato specific KVP's
             SOS_url <- paste(df$source[i],"service=SOS&version=2.0",
-                         "&request=GetObservation",
-                         "&featureOfInterest=",df$SiteName[i],
-                         "&procedure=Cmd.P",
-                         "&observedProperty=Discharge",
-                         sep="")
+                             "&request=GetObservation",
+                             "&featureOfInterest=",df$SiteName[i],
+                             "&procedure=Cmd.P",
+                             "&observedProperty=Discharge",
+                             sep="")
         } else{
             ## Using the minimal Hilltop KVPs
             SOS_url <- paste(df$source[i],"service=SOS",
-                         "&request=GetObservation",
-                         "&featureOfInterest=",df$SiteName[i],
-                         "&observedProperty=",measurement,
-                         sep="")
+                             "&request=GetObservation",
+                             "&featureOfInterest=",df$SiteName[i],
+                             "&observedProperty=",measurement,
+                             sep="")
         }  
         
         SOS_url <- gsub(" ","%20",SOS_url)
@@ -132,7 +132,7 @@ rcLastTVP <- function(df,measurement){
         #                   &temporalFilter=om:phenomenonTime,2014-01-28T15:00:00/2014-01-29T15:00:00
         
         
-        cat(SOS_url,"\n")
+        #cat(SOS_url,"\n")
         
         result = tryCatch({
             getData.xml <- xmlInternalTreeParse(SOS_url)
@@ -155,31 +155,56 @@ rcLastTVP <- function(df,measurement){
             xmltop <- xmlRoot(getData.xml)
             
             if(xmlName(xmltop)!="ExceptionReport"){
-                wml2time<-sapply(getNodeSet(getData.xml,"//wml2:time"),xmlValue)
-                wml2value<-sapply(getNodeSet(getData.xml,"//wml2:value"),xmlValue)
+                if(length(getNodeSet(getData.xml,"//wml2:point"))!=0){
+                    wml2time<-sapply(getNodeSet(getData.xml,"//wml2:time"),xmlValue)[1]
+                    wml2value<-sapply(getNodeSet(getData.xml,"//wml2:value"),xmlValue)[1]
+                    #cat(wml2time,"\n")
+                    #cat(wml2value,"\n")
+                } else {wml2time<-NA
+                        wml2value<-NA}
             }
             
             if(i==1){
-                wml2Time <- c(wml2time)
-                wml2Value <- c(wml2value) 
-            } else {
-                wml2Time1 <- c(wml2time)
-                wml2Value1 <- c(wml2value) 
+                wml2_Time <- wml2time
+                wml2_Value <-as.numeric(wml2value)
+                ## Horizons stores flow as L/s. Others store it as m3/s. The following line adusts Horizons value
+                if(df$source[i]=="http://hilltopserver.horizons.govt.nz/data.hts?"){wml2_Value <- wml2_Value/1000}
+                ## Recoding negative values to NA
+                if(!anyNA(wml2_Value)){
+                    if(wml2_Value < 0){wml2_Value <- NA}
+                }
                 
-                wml2Time <- c(wml2Time,wml2Time1)
-                wml2Value <- c(wml2Value,wml2Value1) 
-            }            
+            } else {
+                wml2_Time1 <- wml2time
+                wml2_Value1 <- as.numeric(wml2value)
+                ## Horizons stores flow as L/s. Others store it as m3/s. The following line adusts Horizons value
+                if(df$source[i]=="http://hilltopserver.horizons.govt.nz/data.hts?"){wml2_Value1 <- wml2_Value1/1000}
+                ## Recoding negative values to NA
+                if(!anyNA(wml2_Value1)){
+                    if(wml2_Value1 < 0){wml2_Value1 <- NA}
+                }
+                
+                wml2_Time <- c(wml2_Time,wml2_Time1)
+                wml2_Value <- c(wml2_Value,wml2_Value1) 
+                rm(wml2_Time1,wml2_Value1)
+            }
+            rm(SOS_url,wml2time,wml2value)
         })
         
     }
-        
-
-    #Append each measurements output vector to the data.frame as a new column
-    df[,length(df)+1] <- wml2Time  ### Add wml2Time
-    colnames(df)[length(df)] <-  c("DateTime")
-    df[,length(df)+1] <- wml2Value  ### Add wml2Value
-    colnames(df)[length(df)] <-  c("Value")
     
+    
+    ## Append each measurements output vector to the data.frame as a new column
+    
+    # DateTime inclusion and conversion to POSIXlt DateTime
+    df[,length(df)+1] <- wml2_Time  ### Add wml2Time
+    colnames(df)[length(df)] <-  c("DateTime")
+    df$DateTime<-strptime(df$DateTime,"%Y-%m-%dT%H:%M:%S")
+    
+    # Measurement value. Data type set to numeric when vector created.
+    df[,length(df)+1] <- wml2_Value  ### Add wml2Value
+    colnames(df)[length(df)] <-  c("Value")
+        
     return(df)
 }
 
@@ -238,7 +263,7 @@ MakeMap <- function(df){
 
 
 
-TimeSeriesSlider <- function(df,stn,measurement){
+TimeSeriesSlider <- function(df,stn,measurement,duration){
 
         
         if(substrRight(df$source[stn],1)=="&"){
@@ -260,8 +285,8 @@ TimeSeriesSlider <- function(df,stn,measurement){
         }  
         
         # Adding temporalFilter to SOS call
-        #SOS_url <- paste(SOS_url,"&temporalFilter=om:phenomenonTime,P7D")
-        SOS_url <- paste(SOS_url,"&temporalFilter=om:phenomenonTime,2014-08-01T00:00:00/2014-08-08T00:00:00",sep="")
+        #SOS_url <- paste(SOS_url,"&temporalFilter=om:phenomenonTime",duration)
+        SOS_url <- paste(SOS_url,"&temporalFilter=om:phenomenonTime,",duration,sep="")
         
         #Replacing spaces with %20 in url
         SOS_url <- gsub(" ","%20",SOS_url)    
@@ -301,13 +326,13 @@ TimeSeriesSlider <- function(df,stn,measurement){
         })
         
         # data review
-        df_P7D <- data.frame(wml2_Time,wml2_Value)
-        names(df_P7D) <- c("Date","Value")
-        df_P7D$Date <- strptime(df_P7D$Date,format="%Y-%m-%dT%H:%M:%S")
-        summary(df_P7D)
+        df_PxD <- data.frame(wml2_Time,wml2_Value)
+        names(df_PxD) <- c("Date","Value")
+        df_PxD$Date <- strptime(df_PxD$Date,format="%Y-%m-%dT%H:%M:%S")
+        summary(df_PxD)
         
         # gvisChart of some sort
-        AnnoTimeLine  <- gvisAnnotatedTimeLine(df_P7D, 
+        AnnoTimeLine  <- gvisAnnotatedTimeLine(df_PxD, 
                                                datevar="Date",
                                                numvar="Value", 
                                                options=list(displayAnnotations=FALSE,
@@ -315,6 +340,123 @@ TimeSeriesSlider <- function(df,stn,measurement){
         plot(AnnoTimeLine)
             
 }
+
+USGS_chart <- function(url){
+        
+    result = tryCatch({
+        getData.xml <- xmlInternalTreeParse(url)
+        
+    }, warning = function(w) {
+        
+    }, error = function(e) {
+        
+        wml2Time <- NA
+        wml2Value <- NA 
+        
+    }, finally = {
+        xmltop <- xmlRoot(getData.xml)
+        
+        if(xmlName(xmltop)!="ExceptionReport"){
+            if(length(getNodeSet(getData.xml,"//wml2:point"))!=0){
+                wml2time<-sapply(getNodeSet(getData.xml,"//wml2:time"),xmlValue)
+                wml2value<-sapply(getNodeSet(getData.xml,"//wml2:value"),xmlValue)
+            } else {wml2time<-NA
+                    wml2value<-NA
+            }
+        }
+        
+        wml2_Time <- wml2time
+        wml2_Value <-as.numeric(wml2value)
+        ## Recoding negative values to NA
+        if(!anyNA(wml2_Value)){
+            if(wml2_Value < 0){wml2_Value <- NA}
+        }
+        
+    })
+    
+    # data review
+    df_PxD <- data.frame(wml2_Time,wml2_Value)
+    names(df_PxD) <- c("Date","Value")
+    df_PxD$Date <- strptime(df_PxD$Date,format="%Y-%m-%dT%H:%M:%S")
+    
+    # gvisChart of some sort
+    AnnoTimeLine  <- gvisAnnotatedTimeLine(df_PxD, 
+                                           datevar="Date",
+                                           numvar="Value", 
+                                           options=list(displayAnnotations=FALSE,
+                                                        width="600px", height="350px"))
+    plot(AnnoTimeLine)
+    
+}
+
+MakeAFlowMap <- function(df){
+    
+    df <- na.omit(df)
+    
+    # Setting variables that specify how map output will be displayed
+    maxValue=4000
+    colValue="royalblue"
+    legendTitle="Flow m3/s"
+    
+    
+    #Resizing variable to a 0-100 range for plotting and putting this data into
+    # df$StdValue column.
+    df$StdValue<-(100/maxValue)*df$Value
+    
+    #===================================================================================================
+    # Define the markers:
+    df.markers <- cbind.data.frame( lat=df$Lat, lon=df$Lon, 
+                                    size=rep('tiny', length(df$Lat)), col=colors()[1:length(df$Lat)], 
+                                    char=rep('',length(df$Lat)) )
+    # Get the bounding box:
+    bb <- qbbox(lat = df[,"Lat"], lon = df[,"Lon"])
+    num.mirrors <- 1:dim(df.markers)[1] ## to visualize only a subset of the cran.mirrors
+    maptype <- c("roadmap", "mobile", "satellite", "terrain", "hybrid", "mapmaker-roadmap", "mapmaker-hybrid")[4]
+    
+    # Download the map (either jpg or png): 
+    MyMap <- GetMap.bbox(bb$lonR, bb$latR, destfile = paste("Map_", maptype, ".png", sep=""), GRAYSCALE=F, maptype = maptype)
+    # Plot:
+    
+    
+    # Controlling symbol size
+    symPower=2/10
+    txtPower=0.009
+    zeroAdd=0.05
+    transp=0
+    
+    
+    
+    PlotOnStaticMap(MyMap,lat = df.markers[num.mirrors,"lat"], lon = df.markers[num.mirrors,"lon"], 
+                    cex=((df$StdValue)+zeroAdd)^symPower, pch=19, col=colValue, add=F)
+    
+    
+    showPointLabels <- "No"
+    #if(showPointLabels=="Yes")
+    #{
+    #  TextOnStaticMap(MyMap,lat = df.markers[num.mirrors,"lat"], lon = df.markers[num.mirrors,"lon"], 
+    #                cex=(df$Value)^txtPower, labels=as.character(round(df$Value,0)), col="white", add=T)
+    #} else {
+    #  ## turn legend on if label not chosen
+    #  legend(x=-310,y=278, c("Low","Medium","High"), title=legendTitle, pch=c(20), cex=1.2,
+    #         pt.cex=c((5+zeroAdd)^symPower,(50+zeroAdd)^symPower,(95+zeroAdd)^symPower), text.col="white", bty="n", col=colValue, y.intersp=1.2)
+    #  
+    #}
+    
+    ## Coloured Rect for Title
+    rect(-320,280,320,320,col="cornflowerblue")
+    
+    
+    ##==========================================    
+    ## Requires pixmap package
+    #hrclogo<-read.pnm("hrclogo_small.pnm")
+    #addlogo(hrclogo,c(-290,-190),c(-200,-130))
+    ##==========================================    
+    
+    text(0,300,labels=c("Flow stations"),cex=2.5, col="white")
+    text(0,-300,labels=c("Horizons Regional Council Disclaimer Applies"),cex=0.7)
+    
+}
+
 
 
 
